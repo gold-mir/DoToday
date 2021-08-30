475a0dfaf4b57c4c8b2d9dcdcfd9dd43f6ba4188
gold-mir <miranda.keith.42@gmail.com>
2021-08-30 00:25:16 -0600

    set up background tasks

--- App.tsx
+++ App.tsx
@@ -1,41 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Modal, Button, TouchableOpacity, TouchableWithoutFeedback } from 'react-native'
+import { StyleSheet, View, Button } from 'react-native'
 
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import * as TaskManager from 'expo-task-manager'
-import * as BackgroundFetch from 'expo-background-fetch'
-import * as Notifications from 'expo-notifications'
 import TaskDB from './resources/TaskDB'
 import { Task, generateDailyTasks } from './resources/task'
 
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer'
 
 import TaskListScreen from './components/TaskListScreen'
 import TaskEntryScreen from './components/TaskEntryScreen'
 import TaskManagerScreen from './components/TaskManagerScreen'
 
+import { StorageKeys, getTomorrow, storeNewDailyTasks, setupBackgroundTasks } from './resources/backgroundTasks'
+
+
 const db = TaskDB.getConnection()
 const Drawer = createDrawerNavigator()
-
-export const StorageKeys = {
-  dailyTasks: 'daily-tasks',
-  nextNewListDate: 'next-date'
-}
 
 interface State {
   tasks: Task[]
   dailyTasks: Task[]
-}
-
-async function storeNewDailyTasks(tasks: Task[]): Promise<number[]> {
-  let ids: number[] = []
-  ids = tasks.map((task) => task.id)
-  await AsyncStorage.setItem(StorageKeys.dailyTasks, JSON.stringify(ids))
-  return ids
 }
 
 function getDailyTasksFromList(tasks: Task[], ids: number[]): Task[]{
@@ -52,15 +39,9 @@ function getDailyTasksFromList(tasks: Task[], ids: number[]): Task[]{
   return output
 }
 
-function getTomorrow(today: Date){
-  let tomorrow = new Date(today)
-  tomorrow.setHours(0,0,0,0)
-  tomorrow.setDate(tomorrow.getDate() + 1)
-  return tomorrow
-}
+setupBackgroundTasks()
 
 function App (): JSX.Element | null {
-
   const [taskEntryVisible, setTaskEntryVisible] = useState<boolean>(false)
   const [state, setState] = useState<State>({tasks: [], dailyTasks: []})
 
@@ -75,15 +56,13 @@ function App (): JSX.Element | null {
     let dailyTaskIds: number[] = dailyTasksString? JSON.parse(dailyTasksString) : null
     
     let now = new Date()
-    console.log(dailyTaskIds)
+    // console.log(dailyTaskIds)
 
     if(!dailyTaskIds || !lastDate || lastDate < now) {
-      console.log(`Don't have daily tasks for ${now}, generating`)
       dailyTasks = generateDailyTasks(tasks, 5)
       storeNewDailyTasks(dailyTasks)
       AsyncStorage.setItem(StorageKeys.nextNewListDate, JSON.stringify(getTomorrow(now)))
     } else {
-      console.log('have daily tasks, getting')
       dailyTasks = getDailyTasksFromList(tasks, dailyTaskIds)
     }
 
@@ -150,71 +129,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   }
 })
-
-// const BACKGROUND_TASK_TEST = 'background-test'
-
-// Notifications.setNotificationHandler({
-//   handleNotification: async () => {
-//     return {
-//       shouldShowAlert: true,
-//       shouldPlaySound: false,
-//       shouldSetBadge: true
-//     }
-//   }
-// })
-
-// TaskManager.defineTask(BACKGROUND_TASK_TEST, async () => {
-
-//   console.log('starting background task')
-
-//   let now = new Date(Date.now())
-//   let storedJson = await AsyncStorage.getItem('storedText')
-
-//   let storedText = storedJson ? JSON.parse(storedJson) : ''
-
-//   console.log(`did task at ${now}`)
-
-//   Notifications.scheduleNotificationAsync({
-//     content: {
-//       title: 'Did a background notification!',
-//       body: `Current time is ${now.getHours() > 12 ? now.getHours() - 12 : now.getHours()}:${now.getMinutes()} ${now.getHours() < 12? 'AM' : 'PM'}\n
-//       Have Stored Text: ${storedText}`
-//     },
-//     trigger: null
-//   })
-
-//   return BackgroundFetch.Result.NewData
-// })
-
-// async function registerTaskNew() {
-//   let isRegistered: boolean = await TaskManager.isTaskRegisteredAsync(BACKGROUND_TASK_TEST)
-//   let registeredTasks: number;
-
-//   if(isRegistered) {
-//     await BackgroundFetch.unregisterTaskAsync(BACKGROUND_TASK_TEST)
-//     registeredTasks = (await TaskManager.getRegisteredTasksAsync()).length
-//     console.log(`unregistered task, ${registeredTasks} total registered tasks`)
-//   }
-//   await BackgroundFetch.registerTaskAsync(BACKGROUND_TASK_TEST, {
-//     minimumInterval: 120,
-//     stopOnTerminate: false,
-//     startOnBoot: true
-//   })
-//   registeredTasks = (await TaskManager.getRegisteredTasksAsync()).length
-//   console.log(`Registered Task. ${registeredTasks} total tasks registered.`)
-// }
-
-// async function unregisterTask() {
-//   let isRegistered: boolean = await TaskManager.isTaskRegisteredAsync(BACKGROUND_TASK_TEST)
-//   if(isRegistered) {
-//     await BackgroundFetch.unregisterTaskAsync(BACKGROUND_TASK_TEST)
-//     let registeredTasks = (await TaskManager.getRegisteredTasksAsync()).length
-//     console.log(`unregistered task, ${registeredTasks} total registered tasks`)
-//   } else {
-//     console.log('Task not registered.')
-//   }
-// }
-
-// registerTaskNew()
-// unregisterTask()
 export default App;
---
+++ resources/backgroundTasks.ts
@@ -0,0 +1,80 @@
+import * as TaskManager from 'expo-task-manager'
+import * as BackgroundFetch from 'expo-background-fetch'
+import * as Notifications from 'expo-notifications'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import TaskDB from './TaskDB'
+import { generateDailyTasks, Task } from './task'
+
+const BACKGROUND_DAILYTASK_GENERATOR = 'daily-task-generator'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: true
+    }
+  }
+})
+
+
+export const StorageKeys = {
+    dailyTasks: 'daily-tasks',
+    nextNewListDate: 'next-date'
+  }
+  
+  export async function storeNewDailyTasks(tasks: Task[]): Promise<number[]> {
+    let ids: number[] = []
+    ids = tasks.map((task) => task.id)
+    await AsyncStorage.setItem(StorageKeys.dailyTasks, JSON.stringify(ids))
+    return ids
+  }
+  
+  export function getTomorrow(today: Date){
+    let tomorrow = new Date(today)
+    tomorrow.setHours(0,0,0,0)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    return tomorrow
+  }
+
+export function setupBackgroundTasks(){
+    // console.log('uwu')
+    TaskManager.defineTask(BACKGROUND_DAILYTASK_GENERATOR, async () => {
+        let currentTime: Date = new Date()
+        let nextNewTaskDateString = await AsyncStorage.getItem(StorageKeys.nextNewListDate)
+        let nextNewTaskDate = nextNewTaskDateString? JSON.parse(nextNewTaskDateString) : null
+
+        if(!nextNewTaskDate || nextNewTaskDate < currentTime){
+            const db = TaskDB.getConnection()
+            let tasks: Task[] = await db.getAllTasks()
+            let dailyTasks = generateDailyTasks(tasks, 5)
+            await storeNewDailyTasks(dailyTasks)
+            await AsyncStorage.setItem(StorageKeys.nextNewListDate, JSON.stringify(getTomorrow(currentTime)))
+
+            let notificationBody: string = ''
+            for(let task of dailyTasks) {
+                notificationBody += `•${task.name}\n`
+            }
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: "Today's Tasks",
+                    body: notificationBody
+                },
+                trigger: new Date(new Date().setHours(8,0,0,0))
+            })
+
+            return BackgroundFetch.Result.NewData
+        } else {
+            return BackgroundFetch.Result.NoData
+        }
+    })
+
+    BackgroundFetch.registerTaskAsync(BACKGROUND_DAILYTASK_GENERATOR, {
+        minimumInterval: 60*60*8,
+        stopOnTerminate: false,
+        startOnBoot: true
+    })
+
+    console.log('registered background tasks')
+}
